72c40da92a175a5c91a4d6c361c067d2789324db
Zyzto <Zyzto@pm.me>
2020-04-19 12:03:27 +0300

    Update App.js
    removed unneeded import and closing tag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Allplants from './Plants/Allplants'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Allplants></Allplants>
+        <Allplants/>
       </div>
     )
   }
